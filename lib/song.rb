584c34f2a0f5301fa04f80af5f96f8e5e70afe5e
Koy Saeteurn <kosatun@aol.com>
2018-07-31 06:36:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,8 @@ class Song
     @artist = artist 
     @genre = genre 
     @@count +=1
-    @@genres 
-    @@artists << artist unless @@artists.include?(artist)
+    @@genres << genre
+    @@artists << artist 
   end   
   
   def self.count
